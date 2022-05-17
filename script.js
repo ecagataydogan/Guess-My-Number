d7f5c4a806311d45a5a061b6f98d4bb56c5474ed
ecagataydogan <ecagataydogan@gmail.com>
2022-05-17 14:06:47 +0300

    Guess number game last stiuation

---
+++ script.js
@@ -0,0 +1,50 @@
+'use strict';
+/*
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.message').textContent = 'Guessed.';
+console.log(document.querySelector('.message').textContent);
+document.querySelector('.guess').value = 23;
+*/
+
+let secretNumber = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = 0;
+const displayMessage = function (message) {
+  document.querySelector('.message').textContent = message;
+};
+
+document.querySelector('.check').addEventListener('click', function () {
+  const guess = Number(document.querySelector('.guess').value);
+
+  if (!guess) {
+    displayMessage('Make a guess between 1-20');
+  } else if (secretNumber === guess) {
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('body').style.backgroundColor = 'green';
+    document.querySelector('.message').textContent = 'Right guess!';
+    if (score > highscore) {
+      highscore = score;
+      document.querySelector('.highscore').textContent = score;
+    }
+  } else if (guess !== secretNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent =
+        guess > secretNumber ? 'Too high!' : 'Too low!';
+      score--;
+      document.querySelector('.score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = 'You loose!';
+      document.querySelector('.score').textContent = 0;
+    }
+  }
+
+  document.querySelector('.again').addEventListener('click', function () {
+    score = 20;
+    document.querySelector('.score').textContent = score;
+    secretNumber = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.message').textContent = 'Start Guessing...';
+    document.querySelector('.guess').value = null;
+    document.querySelector('body').style.backgroundColor = '#222';
+  });
+});
